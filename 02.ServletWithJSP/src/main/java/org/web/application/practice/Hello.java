b09c0c4451d5b82916db1fd2ca8da9bd0c8085ab
Prince <Prince@LAPTOP-KFIKPE1S>
2021-05-23 22:28:38 +0530

    Servlet With JSP Page

---
+++ 02.ServletWithJSP/src/main/java/org/web/application/practice/Hello.java
@@ -0,0 +1,35 @@
+package org.web.application.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Hello extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("Get method called....");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+
+		PrintWriter out = response.getWriter();
+
+		String name = request.getParameter("user");
+		out.print("<br>Hello " + name);
+		out.close();
+		
+
+	}
+	
+	public void destroy() {
+		System.out.println("Destry method called....");
+	}
+
+}
---
+++ 02.ServletWithJSP/target/02.ServletWithJSP-0.0.1-SNAPSHOT/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="/02.ServletWithJSP/Hello" method="get">
+
+	Name:<input type="text" name="user" /> <input type="submit" value="Go" />
+
+</form>
+</body>
+</html>
