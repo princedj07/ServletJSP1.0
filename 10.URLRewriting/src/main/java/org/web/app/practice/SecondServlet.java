d9974f448cc95dc8329a5d45e06908e88f68f531
Prince <Prince@LAPTOP-KFIKPE1S>
2021-05-27 23:13:30 +0530

    URL Rewriting

---
+++ 10.URLRewriting/src/main/java/org/web/app/practice/SecondServlet.java
@@ -0,0 +1,31 @@
+package org.web.app.practice;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SecondServlet extends HttpServlet {
+
+	private static final long serialVersionUID =1L;
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+
+		try {
+
+			response.setContentType("text/html");
+			PrintWriter out = response.getWriter();
+
+			// getting value from the query string
+			String n = request.getParameter("uname");
+			out.print("Hello " + n);
+
+			out.close();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
